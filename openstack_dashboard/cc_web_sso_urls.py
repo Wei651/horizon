dcfecd902ffbacf608f469c021d55feb85384908
Jason Anderson <jasonanderson@uchicago.edu>
2020-08-28 15:46:24 -0500

    Use global login opt-in cookie for federation auth
    This updates the login method detection to use the global opt-in cookie
    set by the Chameleon user portal. Because reading a cookie requires
    access to the request object, a few function signatures had to change to
    make this possible.
    
    This works by telling Horizon that there is no default redirect URL if
    this cookie is set; that default URL was what was providing the
    automatic redirect to Portal for the legacy SSO.

--- openstack_dashboard/cc_web_sso_urls.py
+++ openstack_dashboard/cc_web_sso_urls.py
@@ -1,15 +1,7 @@
-from django.conf import settings
 from django.conf.urls import url
-from django import http as django_http
 
 from openstack_auth import utils
 from openstack_auth import views
-
-
-def request_new_login(request):
-    res = django_http.HttpResponseRedirect(settings.LOGIN_URL)
-    res.set_cookie(utils.FORCE_WEBSSO_CHOICES_COOKIE, '1', max_age=60*60*24)
-    return res
 
 
 urlpatterns = []
@@ -19,5 +11,4 @@ urlpatterns = []
 if utils.is_websso_enabled():
     urlpatterns += [
         url(r"^ccwebsso/$", views.websso, name='chameleon_websso'),
-        url(r"^new-login-flow/$", request_new_login, name='chameleon_new_login'),
     ]
